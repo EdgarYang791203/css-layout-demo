54391192e6dcb8d00bd94f9efdcb7b4c411f9125
hankyang <73307hank@gmail.com>
2025-02-06 20:13:52 +0800

    github 布署失敗

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,6 +12,18 @@ export default defineConfig(({ mode }) => {
     plugins: [vue()],
     define: {
       __APP_ENV__: env.APP_ENV,
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+      watch: {
+        usePolling: true, // 在某些 Docker 和文件系統配置中，這可以幫助改善文件變化檢測
+      },
     },
   };
 });
